87f9a27550975ad28992c683aa66bdefe8bc652d
chenshengli <chenshengli@utry.cn>
2018-06-26 17:35:23 +0800

    提交eureka初始化

---
+++ microservice-spring-cloud/microservice-consumer-moive/src/main/java/com/sunny/cloud/MicroserviceConsumerMoiveApplication.java
@@ -0,0 +1,19 @@
+package com.sunny.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MicroserviceConsumerMoiveApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConsumerMoiveApplication.class, args);
+	}
+}
